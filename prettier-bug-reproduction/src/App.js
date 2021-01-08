df6151fe8fb42ae5dc134165643173b0127e03cc
Curran Kelleher <curran.kelleher@gmail.com>
2021-01-08 10:09:22 -0500

    Attempt to reproduce bug

--- prettier-bug-reproduction/src/App.js
+++ prettier-bug-reproduction/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import prettier from 'prettier/standalone';
+import parserHTML from 'prettier/parser-html';
+const plugins = [parserHTML];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Example from https://prettier.io/playground/
+const unformattedHTML = `<!DOCTYPE html>
+<HTML CLASS="no-js mY-ClAsS">
+  <HEAD>
+    <META CHARSET="utf-8">
+    <TITLE>My tITlE</TITLE>
+    <META NAME="description" content="My CoNtEnT">
+  </HEAD>
+  <body>
+    <P>Hello world!<BR> This is HTML5 Boilerplate.</P>
+    <SCRIPT>
+      window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+      ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
+    </SCRIPT>
+    <SCRIPT src="https://www.google-analytics.com/analytics.js" ASYNC DEFER></SCRIPT>
+  </body>
+</HTML>`;
 
+const formattedHTML = prettier.format(unformattedHTML, {
+  parser: 'html',
+  plugins
+});
+
+const App = () => <pre>{formattedHTML}</pre> 
 export default App;
